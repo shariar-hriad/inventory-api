532f4304bc30cf20cb87d383afbc43023a06455d
shariar-hriad <shariarhossainriad@gmail.com>
2024-05-08 00:18:15 +0600

    Intialization

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 const port = process.env.PORT || 5060
+
+// middleware for application
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(cors())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.status(200).json({ messahe: 'Express on Vercell' })
