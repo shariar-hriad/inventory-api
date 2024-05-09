15012b919e0b8299321bfd801af85026d83567fa
Shariar Hossain Riad <76766167+shariar-hriad@users.noreply.github.com>
2024-05-09 23:20:33 +0600

    Fix

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const hpp = require('hpp')
 const connectDB = require('./config/conn')
 const { notFound, errorHandler } = require('./middleware/error.middleware')
 const customerRoute = require('./routes/customer.route')
@@ -16,11 +19,14 @@ const app = express()
 const port = process.env.PORT || 5060
 
 // middleware for application
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors())
-app.use(morgan('dev'))
+app.use(mongoSanitize())
+app.use(helmet())
+app.use(hpp())
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Express on Vercell' })
@@ -36,3 +42,5 @@ app.use(errorHandler)
 app.listen(port, () => {
     console.log('Running on port ' + port)
 })
+
+module.exports = app
